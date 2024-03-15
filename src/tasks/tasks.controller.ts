718b93508d94851c2017eb1c7e5d0173909020ec
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-03-16 05:27:21 +1100

    add get, get by id, delete endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task-dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,5 +10,17 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+  @Get('/:id')
+  getTaskById(@Param() id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+  @Delete('/:id')
+  deleteTask(@Param() id: string): void {
+    this.tasksService.deleteTask(id);
   }
 }
